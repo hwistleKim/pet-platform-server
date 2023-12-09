a9ee6011c21fb1bd6fb62d24452f70fba7580d38
hwistleKim <hwistleKim@gmail.com>
2023-12-09 16:38:23 +0900

    test_1_20231209

---
+++ life-system/src/main/java/com/example/lifesystem/LifeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeSystemApplication.class, args);
+	}
+
+}
---
+++ life-system/src/test/java/com/example/lifesystem/LifeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
